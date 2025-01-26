ceb4558e97307edb1784a1e21bea563fe9508655
uto8 <70425214+uto8@users.noreply.github.com>
2025-01-26 14:08:16 +0900

    log追加

--- src/app/(unauthed)/auth/sign_in/actions.ts
+++ src/app/(unauthed)/auth/sign_in/actions.ts
@@ -7,13 +7,17 @@ export const loginWithCredentials = async({email, password}:{
   password: string;
 }) => {
   try{
+    console.log("response==============")
     const response = await signIn("credentials", {
       email,
       password,
       redirect: false
     })
+    console.log("response")
+    console.log(response);
     return response;
   }catch(e){
+    console.log(e);
     throw e;
   }
 }
