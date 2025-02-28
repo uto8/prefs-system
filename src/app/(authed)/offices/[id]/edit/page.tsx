d8702e59a287f1712e10f666d05d21ec77e55056
uto8 <70425214+uto8@users.noreply.github.com>
2025-02-28 09:17:50 +0900

    店舗編集

--- src/app/(authed)/offices/[id]/edit/page.tsx
+++ src/app/(authed)/offices/[id]/edit/page.tsx
@@ -16,7 +16,6 @@ import { useToast } from '@/hooks/use-toast';
 const formSchema = z.object({
   name: z.string().nonempty("名前は必須項目です"),
   email: z.string().email("メールアドレスの形式が無効です").nonempty("メールアドレスは必須項目です"),
-  password: z.string().min(5, "5文字以上で入力してください").nonempty("パスワードは必須項目です"),
   phoneNumber: z.string().nonempty("電話番号は必須項目です"),
 })
 
@@ -71,7 +70,6 @@ export default function EditOffice() {
       office: {
         name: data.name,
         email: data.email,
-        password: data.password,
         phoneNumber: data.phoneNumber
       }
     })
@@ -86,15 +84,6 @@ export default function EditOffice() {
     <>
       <div className="sm:flex sm:items-center mb-8">
         <TitleComponent title="店舗編集"/>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <Link
-            href="#"
-            onClick={router.back}
-            className="block rounded-md bg-[#0054ac] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-          店舗一覧に戻る
-          </Link>
-        </div>
       </div>
       <Form {...form}>
         <form
@@ -126,21 +115,6 @@ export default function EditOffice() {
                 </FormLabel>
                 <FormControl>
                   <Input {...field} type="text" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({field}) => (
-              <FormItem>
-                <FormLabel>
-                パスワード
-                </FormLabel>
-                <FormControl>
-                  <Input {...field} type="password" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
