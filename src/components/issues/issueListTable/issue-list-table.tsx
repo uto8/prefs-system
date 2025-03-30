ca01604dc86947d4dd2497ae3ce19adffa6251f9
uto8 <70425214+uto8@users.noreply.github.com>
2025-03-31 08:14:26 +0900

    受付追加

--- src/components/issues/issueListTable/issue-list-table.tsx
+++ src/components/issues/issueListTable/issue-list-table.tsx
@@ -246,9 +246,9 @@ export default function IssueListTable({issues: issues}: {
                       </a>
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      <a href={`/receptions/${issue.reception.id}/show`} className="text-indigo-600 hover:text-indigo-900">
+                      {issue.reception.id?<a href={`/receptions/${issue.reception.id}/show`} className="text-indigo-600 hover:text-indigo-900">
                         {issue.reception?issue.reception.name:null}
-                      </a>
+                      </a>: <>-</>}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       <Dialog>
