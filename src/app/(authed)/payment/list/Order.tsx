0174c56dfcb4ed1fe844a7be2ebf6d0bd57a4b25
uto8 <70425214+uto8@users.noreply.github.com>
2025-05-25 17:22:28 +0900

    loading追加

--- src/app/(authed)/payment/list/Order.tsx
+++ src/app/(authed)/payment/list/Order.tsx
@@ -72,7 +72,9 @@ export default function OrderTab({
   return (
     <>
     {loading ? (
-      <></>
+      <div className="flex justify-center" aria-label="読み込み中">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+    </div>
     ) : (
     <>
     <table className="w-full text-left">
